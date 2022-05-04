b2901da4de94380577096ea7c649e3a4e2dc4733
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-04 07:30:30 +0100

    Update Monte_Carlo.cpp
    Add multithreading support for monte carlo simulations.

--- Options-Pricing-Library/Monte_Carlo.cpp
+++ Options-Pricing-Library/Monte_Carlo.cpp
@@ -1,6 +1,6 @@
 #include "Options-Pricing-Lib.h"
 
-float Monte_Carlo(const int& number_of_simulations, const int& time_steps_per_simulation, const float& strike, const float& risk_free_interest_rate, const float& continuous_dividend_yield, const float& time_until_option_expiry, const float& volatility, const float& spot_price_of_underlying, float (*payoff_function)(const float& strike, const std::vector<float>& array)) {
+void Monte_Carlo_Core(const int number_of_simulations, const int time_steps_per_simulation, const float strike, const float risk_free_interest_rate, const float continuous_dividend_yield, const float time_until_option_expiry, const float volatility, const float spot_price_of_underlying, float (*payoff_function)(const float& strike, const std::vector<float>& array), int thread_index, std::vector<float>& thread_output_vector) {
 
     std::vector<float> array(time_steps_per_simulation);
 
@@ -12,5 +12,56 @@ float Monte_Carlo(const int& number_of_simulations, const int& time_steps_per_si
         sum_payoff += (*payoff_function)(strike, array) * discount_factor;
     }
 
-    return sum_payoff / number_of_simulations;
+    thread_output_vector[thread_index] = sum_payoff;
+}
+
+
+float Monte_Carlo(const int number_of_simulations, const int time_steps_per_simulation, const float& strike, const float risk_free_interest_rate, const float& continuous_dividend_yield, const float time_until_option_expiry, const float volatility, const float spot_price_of_underlying, float (*payoff_function)(const float& strike, const std::vector<float>& array), int thread_override = -1) {
+    
+    int total_threads = std::thread::hardware_concurrency();
+    
+    if (thread_override != -1) {
+        total_threads = thread_override;
+    }
+    
+    int simulations_per_thread = number_of_simulations / total_threads;
+    int simulations_remaining = number_of_simulations;
+    std::vector<float> thread_output_vector(total_threads);
+    std::vector<std::thread> threads;
+    
+    int thread_index = 0;
+    
+    while(simulations_remaining > 0) {
+        
+        if (simulations_remaining < simulations_per_thread) {
+            simulations_per_thread = simulations_remaining;
+        }
+        
+        threads.push_back(std::thread(Monte_Carlo_Core,
+                                      simulations_per_thread,
+                                      time_steps_per_simulation,
+                                      strike,
+                                      risk_free_interest_rate,
+                                      continuous_dividend_yield,
+                                      time_until_option_expiry,
+                                      volatility,
+                                      spot_price_of_underlying,
+                                      payoff_function,
+                                      thread_index,
+                                      std::ref(thread_output_vector)));
+        thread_index++;
+        simulations_remaining -= simulations_per_thread;
+    }
+
+    for(auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+    
+    float sum = std::accumulate(thread_output_vector.begin(),
+                                thread_output_vector.end(),
+                                0.0);
+    
+    return sum / number_of_simulations;
 }
