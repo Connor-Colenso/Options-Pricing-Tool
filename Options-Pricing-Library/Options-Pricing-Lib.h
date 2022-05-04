3ba7da336d7dc2588bdf7787dd4db6412e552113
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-04 07:31:40 +0100

    Update Options-Pricing-Lib.h
    Multithreading support

--- Options-Pricing-Library/Options-Pricing-Lib.h
+++ Options-Pricing-Library/Options-Pricing-Lib.h
@@ -32,4 +32,5 @@ namespace Payoff {
 float Arithmetic_Average(std::vector<float> array);
 float Geometric_Average(std::vector<float> array);
 
-float Monte_Carlo(const int& number_of_simulations, const int& time_steps_per_simulation, const float& strike, const float& risk_free_interest_rate, const float& continuous_dividend_yield, const float& time_until_option_expiry, const float& volatility, const float& spot_price_of_underlying, float (*payoff_function)(const float& strike, const std::vector<float>& array));
+float Monte_Carlo(const int number_of_simulations, const int time_steps_per_simulation, const float& strike, const float risk_free_interest_rate, const float& continuous_dividend_yield, const float time_until_option_expiry, const float volatility, const float spot_price_of_underlying, float (*payoff_function)(const float& strike, const std::vector<float>& array), int thread_override = -1);
+	
