1a24674ac82c8093c0d76b0699921769c71ccb1c
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-04 07:33:59 +0100

    Update Options-Pricing-App.cpp

--- Options-Pricing-App/Options-Pricing-App.cpp
+++ Options-Pricing-App/Options-Pricing-App.cpp
@@ -10,43 +10,5 @@
 #include <vector>
 
 int main() {
-
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto time_1 = high_resolution_clock::now();
-
-    int number_of_threads = std::thread::hardware_concurrency();
-
-    uint64_t number_of_elements = 1000 * 1000 * 1000;
-    uint64_t step = number_of_elements / number_of_threads;
-    std::vector<std::thread> threads;
-    std::vector<uint64_t> partial_sums(number_of_threads);
-
-    for (uint64_t i = 0; i < number_of_threads; i++) {
-        threads.push_back(std::thread([i, &partial_sums, step] {
-            for (uint64_t j = i * step; j < (i + 1) * step; j++) {
-                partial_sums[i] += j;
-            }
-            }));
-    }
-
-    for (std::thread& t : threads) {
-        if (t.joinable()) {
-            t.join();
-        }
-    }
-
-    uint64_t total = std::accumulate(partial_sums.begin(), partial_sums.end(), uint64_t(0));
-
-    auto time_2 = high_resolution_clock::now();
-
-    duration<double, std::milli> ms_double = time_2 - time_1;
-
-    std::cout << ms_double.count() << "ms ";
-    std::cout << " Total: " << total << std::endl;
-
     return 0;
 }
