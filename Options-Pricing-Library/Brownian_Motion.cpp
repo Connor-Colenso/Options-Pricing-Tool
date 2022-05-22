f4a2a224c0672fe6d8141de95994eff735cca8e4
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-22 22:15:31 +0100

    Update Brownian_Motion.cpp

--- Options-Pricing-Library/Brownian_Motion.cpp
+++ Options-Pricing-Library/Brownian_Motion.cpp
@@ -1,31 +1,26 @@
 #include "Options-Pricing-Lib.h"
 
-std::minstd_rand gen(std::random_device{}());
-std::normal_distribution<> d{ 0, 1 };
 
 namespace Utility {
     void Brownian_Motion(std::vector<float>& array) {
         // Sample random values from a standardised normal distribution.
+        std::minstd_rand gen(std::random_device{}());
+        std::normal_distribution<> d{ 0, 1 };
+
         float tmp = 0.0f;
-        float dt = (float) std::sqrt(1.0 / (float)(n));
+        float dt = 1.0f / std::sqrtf(array.size());
 
         for (int i = 0; i < array.size(); ++i) {
             array[i] = tmp;
             tmp += float(d(gen)) * dt;
-            //tmp += float(var_nor()) * dt;
         }
     }
 
     void Geometric_Brownian_Motion(std::vector<float>& array, const float& x0, const float& t, const float& mu, const float& sigma, const float& pre_t_division, const float& pre_calc_drift) {
-		
+
         Brownian_Motion(array);
 
-        float t_counter = 0.0f;
-        float pre_t_division = t / (float)n;
-
-        float pre_calc_drift = mu - float(0.5) * sigma * sigma;
-
-        for (int i = 0; i < array.size(); ++i) {
+        for (int i = 0; i < array.size(); i++) {
             array[i] = x0 * std::exp(pre_calc_drift * pre_t_division * i + array[i] * sigma);
         }
     }
