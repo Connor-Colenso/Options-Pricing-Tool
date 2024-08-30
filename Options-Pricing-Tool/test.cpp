9e100c9fc8ab815b6d714f222879b547d8a6c893
Connor Colenso <colen@CONNORSPC>
2024-08-30 16:34:23 +0100

    Tidying

--- Options-Pricing-Tool/test.cpp
+++ Options-Pricing-Tool/test.cpp
@@ -6,21 +6,22 @@
 #include "../Options-Pricing-Library/Payoff.cpp"
 
 // ---------------------- General Test Setup --------------------------
+namespace {
+    // Monte Carlo Settings.
+    constexpr int time_steps_per_simulation = 10000;
+    constexpr int number_of_simulations = 50000;
 
-// Monte Carlo Settings.
-static constexpr int time_steps_per_simulation = 10000;
-static constexpr int number_of_simulations = 50000;
+    // Option Settings.
+    constexpr float strike = 15.0f;
+    constexpr float risk_free_interest_rate = 0.05f;
+    constexpr float continuous_dividend_yield = 0.01f;
+    constexpr float time_until_option_expiry = 1.0f;
+    constexpr float volatility = 0.04f;
+    constexpr float spot_price_of_underlying = 14.5f;
 
-// Option Settings.
-static constexpr float strike = 15.0f;
-static constexpr float risk_free_interest_rate = 0.05f;
-static constexpr float continuous_dividend_yield = 0.01f;
-static constexpr float time_until_option_expiry = 1.0f;
-static constexpr float volatility = 0.04f;
-static constexpr float spot_price_of_underlying = 14.5f;
-
-// Test Settings
-static constexpr float tolerance = 0.01f;
+    // Test Settings
+    constexpr float tolerance = 0.01f;
+ }
 
 // ---------------------- European Options Tests ----------------------
 
